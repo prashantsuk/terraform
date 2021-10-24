d1d35bc21e9af7399e394ad90c735a7180aee591
prashantsuk <prashantsukhadeve123@gmail.com>
2021-10-24 12:43:53 +0000

    first commit

---
+++ jenkins.tf
@@ -0,0 +1,47 @@
+resource "aws_instance" "jenkins" {
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.web_traffic.name]
+  key_name        = "kluu"
+
+  provisioner "remote-exec" {
+    inline = [
+      "wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -",
+      "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
+      "sudo apt update -qq",
+      "sudo apt install -y default-jre",
+      "sudo apt install -y jenkins",
+      "sudo systemctl start jenkins",
+      "sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080",
+      "sudo sh -c \"iptables-save > /etc/iptables.rules\"",
+      "echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections",
+      "echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections",
+      "sudo apt-get -y install iptables-persistent",
+      "sudo ufw allow 8080",
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file("~/kluu.pem")
+  }
+
+  tags = {
+    "Name"      = "Jenkins_Server"
+    "Terraform" = "true"
+  }
+}
+
+resource "aws_instance" "web" {
+  # puppet instalation
+
+  provisioner "puppet" {
+    server             = aws_instance.puppetmaster.public-subnet
+    server_user        = "ec2-user"
+    extension_requests = {
+      pp_role = "webserver"
+    }
+  }
+}
---
+++ network.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+variable "ingressrules" {
+  type    = list(number)
+  default = [80, 443, 22]
+}
+
+resource "aws_security_group" "web_traffic" {
+  name        = "Allow web traffic"
+  description = "Allow ssh and standard http/https ports inbound and everything outbound"
+
+  dynamic "ingress" {
+    iterator = port
+    for_each = var.ingressrules
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" = "true"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+
+...
+
+}
+
+resource "aws_instance" "jenkins" {
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.web_traffic.name]
+  key_name        = "kluu"
+
+  tags = {
+    "Name"      = "Jenkins_Server"
+    "Terraform" = "true"
+  }
+}
